a61988a608b8e0e5d21b813ce613972f50e69e9f
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-10-31 17:48:44 -0300

    Clase 31/10 v2
    Mas cosas de la clase extra

---
+++ PooTPMundial/Datos/Partidos.java
@@ -0,0 +1,22 @@
+package Datos;
+
+import java.util.LinkedList;
+
+public class Partidos {
+	
+	private int id;
+	private Pais team1;
+	private Pais team2;
+	private int goles;
+	private double duracion;
+	
+	LinkedList<Partidos> partidos = new LinkedList<>();
+	
+	
+	
+}
+
+
+	
+	
+	
