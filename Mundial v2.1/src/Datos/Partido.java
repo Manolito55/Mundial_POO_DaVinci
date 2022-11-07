a2df89642baa1a99bafa61572151bb439b5a55de
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-11-07 18:39:07 -0300

    Clase 7/11 v2
    Maquetacion de los Syso

---
+++ Mundial v2.1/src/Datos/Partido.java
@@ -0,0 +1,66 @@
+package Datos;
+
+public class Partido {
+	
+	private int id=0;
+	private Pais team1;
+	private Pais team2;
+	private int goles1;
+	private int goles2;
+	private double duracion;
+	
+	public Partido( Pais team1, Pais team2, int goles1, int goles2, double duracion) {
+		super();
+		this.id = id++;
+		this.team1 = team1;
+		this.team2 = team2;
+		this.goles1 = goles1;
+		this.goles2 = goles2;
+		this.duracion = duracion;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Pais getTeam1() {
+		return team1;
+	}
+	public void setTeam1(Pais team1) {
+		this.team1 = team1;
+	}
+	public Pais getTeam2() {
+		return team2;
+	}
+	public void setTeam2(Pais team2) {
+		this.team2 = team2;
+	}
+	public int getGoles1() {
+		return goles1;
+	}
+	public void setGoles1(int goles1) {
+		this.goles1 = goles1;
+	}
+	public int getGoles2() {
+		return goles2;
+	}
+	public void setGoles2(int goles2) {
+		this.goles2 = goles2;
+	}
+	public double getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(double duracion) {
+		this.duracion = duracion;
+	}
+	
+	
+	
+}
+
+
+	
+	
+	
