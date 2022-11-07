98e22fdc98bc6cd7fbd7b27b3376a19f03841248
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-11-07 16:51:11 -0300

    Clase 7/11
    Finalizamos la lista de Partidos y pasamos en limpio los metodos

---
+++ Mundial v2/src/Datos/Encargado.java
@@ -0,0 +1,31 @@
+package Datos;
+
+import javax.swing.JOptionPane;
+
+public class Encargado {
+	
+	public static Partido JugarPartido(Pais pais1, Pais pais2) {
+		
+		double tiempo = (double)(Math.random()*8) + 90;
+		
+		int golequipo1 = (int)(Math.random()*4);
+		int golequipo2 = (int)(Math.random()*4);
+		
+		while ((golequipo1 == golequipo2)) {
+			
+			golequipo1 = (int)(Math.random()*4);
+			golequipo2 = (int)(Math.random()*4);
+			
+		}
+			
+		System.out.println("Goles team1 = "+golequipo1+ " - "+ "Goles team2 = "+golequipo2+" - "+"Duracion: "+tiempo);
+		
+			 Partido partidonuevo = new Partido(pais1,pais2,golequipo1,golequipo2,tiempo);
+			 return partidonuevo;
+			
+
+	}
+	
+
+	
+}
---
+++ Mundial v2/src/Datos/Pais.java
@@ -0,0 +1,70 @@
+package Datos;
+
+public class Pais {
+
+	private String nombre;
+	private String grupo;
+	private int goles;
+	private int cantidadpartidos;
+	private boolean calificado;
+	
+	public Pais(String nombre, String grupo, int goles, int cantidadpartidos, boolean calificado) {
+		super();
+		this.nombre = nombre;
+		this.grupo = grupo;
+		this.goles = goles;
+		this.cantidadpartidos = cantidadpartidos;
+		this.calificado = calificado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+
+	public int getGoles() {
+		return goles;
+	}
+
+	public void setGoles(int goles) {
+		this.goles = goles;
+	}
+
+	public int getCantidadpartidos() {
+		return cantidadpartidos;
+	}
+
+	public void setCantidadpartidos(int cantidadpartidos) {
+		this.cantidadpartidos = cantidadpartidos;
+	}
+
+	public boolean isCalificado() {
+		return calificado;
+	}
+
+	public void setCalificado(boolean calificado) {
+		this.calificado = calificado;
+	}
+
+	@Override
+	public String toString() {
+		return "Pais [nombre=" + nombre + ", grupo=" + grupo + ", goles=" + goles + ", cantidadpartidos="
+				+ cantidadpartidos + ", calificado=" + calificado + "]";
+	}
+	
+	
+
+
+}
+
